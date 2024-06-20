46e7cbd8de22d4111c80d90c268654b74ead8919
Ferit Korkmaz <korkmazferit1905@gmail.com>
2024-06-20 18:54:50 +0300

    required properties added

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PhoneBook.Models
 {
     public class Contact
     {
         public int Id { get; set; }
+        [Required]
         public int UserId { get; set; }
+        [Required]
         public string? FirstName { get; set; }
+        [Required]
         public string? LastName { get; set; }
+        [Required]
         public string? PhoneNumber { get; set; }
     }   
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PhoneBook.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required]
         public string? UserName { get; set; }
+        [Required]
         public string? Password { get; set; }
     }   
 }
